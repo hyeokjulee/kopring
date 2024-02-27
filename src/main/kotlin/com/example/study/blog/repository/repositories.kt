a36f03b390dde1e6a1c1c1f54100c52f5a0107f3
HyeokJu Lee <dlhjwwe@naver.com>
2024-02-28 01:42:10 +0900

    Repositories에 findTop10ByOrderBycntDesc() 추가

--- src/main/kotlin/com/example/study/blog/repository/repositories.kt
+++
@@ -1,6 +0,0 @@
-package com.example.study.blog.repository
-
-import com.example.study.blog.entity.Wordcount
-import org.springframework.data.repository.CrudRepository
-
-interface WordRepository : CrudRepository<Wordcount, String>
